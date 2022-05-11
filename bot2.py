6b73636124d12458f4186ed9609a6c67575f31d4
Luca <lucapalminteri02@gmail.com>
2022-05-11 07:33:56 -0300

    Update bot2.py

--- bot2.py
+++ bot2.py
@@ -1,4 +1,4 @@
 import pywhatkit
 
 
-pywhatkit.sendwhatmsg("+5493412169777", "Message sending from Python, bot made by Luca (send only 9:15)",9,15)
+pywhatkit.sendwhatmsg("+xxxxxxxxx-xxxx", "Message sending from Python, bot made by Luca (send only 9:15)",9,15)
